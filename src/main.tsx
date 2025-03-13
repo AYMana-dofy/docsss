107dc9ea597b3f9e7a2b8343fed0d1bcdaa9b475
AYMana-dofy <elghaf.ayman@gmail.com>
2025-03-13 22:29:24 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ TempoDevtools.init();
 
 const basename = import.meta.env.BASE_URL;
 
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter basename={basename}>
